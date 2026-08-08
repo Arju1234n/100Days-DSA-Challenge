6924b795dffc05e4e5fee2f50da4a0ec93f8ac9e
arjun kumar <kumararjun5230@gmail.com>
2026-08-08 21:23:44 +0530

    Add Solution for Longest Common Prefix in 03-Strings

---
+++ 03-Strings/longest_common_prefix.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+
+        string ans = "";
+
+        for (int i = 0; i < strs[0].size(); i++) {
+            char ch = strs[0][i];
+
+            for (int j = 1; j < strs.size(); j++) {
+                if (i >= strs[j].size() || strs[j][i] != ch)
+                    return ans;
+            }
+
+            ans += ch;
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    Solution sol;
+    vector<string> strs = {"flower", "flow", "flight"};
+    cout << sol.longestCommonPrefix(strs) << endl; // Output: "fl"
+    return 0;
+}
